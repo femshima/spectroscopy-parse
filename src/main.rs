98214f62e95c394c5116f18e9e923393256fdf8e
femshima <49227365+femshima@users.noreply.github.com>
2023-07-12 11:34:31 +0900

    multiple spectras

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File};
+use std::{collections::HashMap, error::Error, fs::File};
 
 use ole::{EntryType, Reader};
 
@@ -11,19 +11,38 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("test-data")?;
     let parser = Reader::new(file).unwrap();
 
-    let mut spectra = Spectra::default();
+    let mut spectra_name: HashMap<u32, &str> = HashMap::new();
+    let mut spectras: HashMap<u32, Spectra> = HashMap::new();
     for entry in parser.iterate() {
-        println!("{}", entry);
-        if entry._type() != EntryType::UserStream {
-            continue;
-        }
-        if let Ok(data) = parser.get_entry_slice(entry).as_mut() {
-            spectra.read_from(entry.name(), data)?;
+        println!("{} {:?}", entry, entry.parent_node());
+
+        match entry._type() {
+            EntryType::RootStorage => {
+                spectra_name.insert(entry.id(), "[Root]");
+            }
+            EntryType::UserStorage => {
+                spectra_name.insert(entry.id(), entry.name());
+            }
+            EntryType::UserStream => {
+                let parent_id = entry.parent_node().unwrap();
+                let spectra = spectras.entry(parent_id).or_default();
+
+                if let Ok(data) = parser.get_entry_slice(entry).as_mut() {
+                    spectra.read_from(entry.name(), data)?;
+                }
+            }
+            _ => (),
         }
     }
 
-    dbg!(&spectra.data_info);
-    dbg!(&spectra.y_data.len());
-    // dbg!(spectra);
+    for (id, spectra) in spectras {
+        if spectra.y_data.len() > 0 {
+            dbg!(
+                spectra_name.get(&id).unwrap(),
+                &spectra.data_info,
+                &spectra.y_data.len()
+            );
+        }
+    }
     Ok(())
 }
